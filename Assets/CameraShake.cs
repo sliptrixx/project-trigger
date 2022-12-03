614f3f0500c0e9cf24688a98c9538e01ca60c6a4
Hibnu Hishath <hibnuhishath1@gmail.com>
2022-12-03 11:14:16 -0800

    Implemented camera shake with some atttenuation

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+	// how long the shake should be?
+	float duration = 0;
+
+	// how strong should the shake be?
+	float magnitude;
+
+	// attenuation per second
+	float attenuation;
+
+	// the origin where the object starts
+	Vector3 origin;
+
+	// the offset to apply to the origin to make the camera shake happen
+	Vector3 offset;
+
+	// flip flop tracker
+	bool moveRight = false;
+
+	void Start()
+	{
+		origin = transform.position;
+	}
+
+	void Update()
+	{
+		// if the duration is less than 0, there's nothing to shake
+		if(duration <= 0) { return; }
+
+		// pick a random value that's between 0 and the magnitude as the x offset
+		float x = Random.Range(0, magnitude);
+
+		// calculate the y offset so that's it in a circle with a radius of magnitude
+		float y = Mathf.Sqrt(magnitude * magnitude - x * x);
+
+		// pick the positive/negative axises - x is based on moveRight - y is random
+		x *= moveRight ? 1 : -1;
+		y *= (Random.Range(0.0f, 1.0f) < 0.5f) ? 1 : -1;
+
+		// appply the offset
+		transform.position = origin + new Vector3(x, y, 0);
+
+		// tick down the duration and update magnitude based on the calculated attenuation
+		duration -= Time.deltaTime;
+		magnitude -= attenuation * Time.deltaTime;
+		moveRight = !moveRight;
+	}
+
+	// Shake the camera with the given magnitude for the specified duration
+	public void Shake(float duration, float magnitude)
+	{
+		this.duration = duration;
+		this.magnitude = magnitude;
+		attenuation = magnitude / duration;
+	}
+}
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,10 +7,20 @@ public class Health : MonoBehaviour
 {
 	[SerializeField] protected int hp = 1;
 
+	CameraShake cameraShake = null;
+
+	void Start()
+	{
+		cameraShake = FindObjectOfType<CameraShake>();
+	}
+
 	// Apply damage to the object
 	public void ApplyDamage(int damage)
 	{
 		hp -= damage;
+
+		// apply camera shake as a feedback
+		cameraShake.Shake(0.25f, 0.2f);
 
 		// if the health drops below zero destroy the object
 		if (hp <= 0)
@@ -18,6 +28,9 @@ public class Health : MonoBehaviour
 			// play the explosion sound effect before destroying the object
 			var audioManager = FindObjectOfType<AudioManager>();
 			audioManager?.PlayExplosion(transform.position);
+
+			// Apply a larger camera shake when the enemy is killed
+			cameraShake.Shake(0.4f, 0.4f);
 
 			// destroy the object
 			Destroy(gameObject);
